323d949ed3df7e1bce9e030486957cd22b5a784a
shohjahon1code <suyunovshohjahon08@gmail.com>
2023-04-15 13:14:19 +0500

    responsive fixed

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,38 +1,50 @@
-import React, { useState } from 'react';
-import { HeaderProps } from './header.props';
-import classes from './header.module.css';
-import cn from 'classnames';
-import LogoIcon from '../logo.svg';
-import { IconButton } from '../../components';
-import Sidebar from '../sidebar/sidebar';
-import { motion } from 'framer-motion';
-
+import React, { useState } from "react";
+import { HeaderProps } from "./header.props";
+import classes from "./header.module.css";
+import cn from "classnames";
+import LogoIcon from "../logo.svg";
+import { IconButton } from "../../components";
+import Sidebar from "../sidebar/sidebar";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
-  const [isopen, setIsOpen] = useState<boolean>(false)
+  const [isopen, setIsOpen] = useState<boolean>(false);
 
-  const toggleMenu = () => setIsOpen(prev => !prev)
+  const toggleMenu = () => setIsOpen((prev) => !prev);
 
   const variants = {
     open: {
       opacity: 1,
-      x:0,
+      x: 0,
       transition: {
-        stiffnes:20
-      }
+        stiffnes: 20,
+      },
     },
     close: {
       opacity: 0,
-      x:'100%'
-    }
-  }
+      x: "100%",
+    },
+  };
   return (
     <div className={classes.header} {...props}>
-      <div>Logo</div>
-      <IconButton appearance='white' icon='menu' onClick={toggleMenu} />
-      <motion.div variants={variants} initial={'close'} animate={isopen ? 'open': 'close'} className={classes.mobilemenu}>
+      <Link href={"/"}>
+        <LogoIcon />
+      </Link>
+      <IconButton appearance="white" icon="menu" onClick={toggleMenu} />
+      <motion.div
+        variants={variants}
+        initial={"close"}
+        animate={isopen ? "open" : "close"}
+        className={classes.mobilemenu}
+      >
         <Sidebar />
-        <IconButton appearance='white' icon='close' className={classes.closeIcon} onClick={toggleMenu} />
+        <IconButton
+          appearance="white"
+          icon="close"
+          className={classes.closeIcon}
+          onClick={toggleMenu}
+        />
       </motion.div>
     </div>
   );
